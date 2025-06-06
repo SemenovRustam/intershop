b7cfe851aadc3674c14098914b6c6bb46df926be
rvsemenov <rvsemenov@domclick.ru>
2025-06-06 21:27:03 +0300

    init commit

---
+++ src/test/java/com/praktikum/semenov/intershop/IntershopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikum.semenov.intershop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
