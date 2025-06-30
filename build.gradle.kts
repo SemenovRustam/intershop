81208a7fb7aa5ff5266e9a6fa0b35ff0ab1b19f0
rvsemenov <rvsemenov@domclick.ru>
2025-06-30 17:39:43 +0300

    [+] move to multy  repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,26 @@
-import org.springframework.boot.gradle.tasks.bundling.BootJar
-
 plugins {
-	java
-	id("org.springframework.boot") version "3.5.0"
-	id("io.spring.dependency-management") version "1.1.7"
+	id("org.springframework.boot") version "3.5.0" apply false
+	id("io.spring.dependency-management") version "1.1.7" apply false
 }
 
-group = "com.praktikum.semenov"
-version = "0.0.1-SNAPSHOT"
+allprojects {
+	apply(plugin = "java")
 
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
+	group = "com.praktikum.semenov"
+	version = "0.0.1-SNAPSHOT"
+
+	repositories {
+		mavenCentral()
+	}
+
+	configure<JavaPluginExtension> {
+		toolchain {
+			languageVersion.set(JavaLanguageVersion.of(21))
+		}
 	}
 }
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
-repositories {
-	mavenCentral()
-}
-
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	implementation("org.springframework:spring-webflux")
-	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-	implementation("io.r2dbc:r2dbc-postgresql:0.8.13.RELEASE")
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.liquibase:liquibase-core")
-	runtimeOnly("org.postgresql:postgresql") // JDBC-драйвер
-	implementation("org.mapstruct:mapstruct:1.5.5.Final")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-	testImplementation("io.projectreactor:reactor-test")
-}
-
-tasks.withType<Test> {
-	useJUnitPlatform()
-}
-
-tasks.withType<BootJar> {
-	archiveFileName.set("intershop.jar")
+subprojects {
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
 }
