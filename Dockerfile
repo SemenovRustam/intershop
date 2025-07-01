9f19f3ddd3962cfe0ed5c6ab1e5732bca1af7c12
rvsemenov <rvsemenov@domclick.ru>
2025-07-01 16:42:27 +0300

    [+] fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM openjdk:21-jdk-slim
 
 WORKDIR /app
 
-COPY build/libs/*.jar app.jar
+COPY app/build/libs/*.jar app.jar
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     java
     id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.openapi.generator") version "7.12.0"
 }
 
 group = "com.praktikum.semenov"
@@ -29,6 +30,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework:spring-webflux")
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+//    implementation("io.lettuce:lettuce-core:6.2.4.RELEASE")
+    implementation("com.fasterxml.jackson.core:jackson-databind")
     implementation("io.r2dbc:r2dbc-postgresql:0.8.13.RELEASE")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.liquibase:liquibase-core")
@@ -42,6 +46,10 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
+    //doc
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.2.0")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("io.projectreactor:reactor-test")
 }
@@ -52,9 +60,40 @@ tasks.withType<Test> {
 
 tasks.withType<BootJar> {
     archiveFileName.set("intershop.jar")
+    mainClass.set("com.praktikum.semenov.intershop.IntershopApplication")
 }
-
 springBoot {
     mainClass.set("com.praktikum.semenov.intershop.IntershopApplication")
 }
 
+
+openApiGenerate {
+    generatorName.set("spring")
+    inputSpec.set(layout.projectDirectory.file("src/main/resources/api/openapi.yml").asFile.absolutePath)
+    outputDir.set(layout.buildDirectory.dir("generated").get().asFile.absolutePath)
+    apiPackage.set("com.praktikum.semenov.generated.api")
+    modelPackage.set("com.praktikum.semenov.generated.model")
+    configOptions.set(mapOf(
+        "interfaceOnly" to "true",
+        "useSpringBoot3" to "true",
+        "reactive" to "true",
+        "java8" to "true",
+        "skipValidateSpec" to "true"
+    ))
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("${layout.buildDirectory}/generated/src/main/java")
+        }
+    }
+}
+
+tasks.compileJava {
+    dependsOn(tasks.openApiGenerate)
+}
+
+tasks.jar {
+    enabled = false
+}
